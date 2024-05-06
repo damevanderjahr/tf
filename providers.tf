166480319408fe08bb24e979722dd8531e6d2a73
Dmytro Zharynin <damevanderjahr@gmail.com>
2024-05-06 22:31:44 +0300

    Cleanup code

--- providers.tf
+++ providers.tf
@@ -5,20 +5,13 @@ provider "flux" {
     cluster_ca_certificate = module.gke_cluster.cluster_ca_certificate
   }
   git = {
-    url = "ssh://git@github.com/${var.GITHUB_OWNER}/${var.FLUX_GITHUB_REPO}.git"
+    url = module.github_repository.values.http_clone_url
     ssh = {
       username    = "git"
       private_key = module.tls_private_key.private_key_pem
     }
   }
 }
-
-# provider "github" {
-#   owner = var.GITHUB_OWNER
-#   token = var.GITHUB_TOKEN
-# }
-
-# provider "kind" {}
 
 provider "kubernetes" {
   host                   = module.gke_cluster.host
